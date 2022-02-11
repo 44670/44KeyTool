514902b35eba874e54413ae7b2a704a2a4e4cf85
44670 <44670@users.noreply.github.com>
2022-02-11 20:21:32 +0800

    typo

---
+++ ssh-agent_unix.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/crypto/ssh/agent"
+)
+
+func ServeSSHAgentOnUnixDomainSocket(path string) error {
+	// Start a ssh agent on unix domain socket
+	listener, err := net.Listen("unix", path)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+	log.Println("SSH agent started at ", path)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go func() {
+			// Handle the connection
+			err := agent.ServeAgent(myAgent, conn)
+			log.Println("Agent conn closed:", err)
+		}()
+	}
+}
+
+func RunSSHServer() {
+
+	log.Println(`=== Set the following environment variable before using ssh ===
+	export SSH_AUTH_SOCK=/tmp/44ssh
+============================================================`)
+	// Start a ssh agent on unix domain socket
+	err := ServeSSHAgentOnUnixDomainSocket(`/tmp/44ssh`)
+	if err != nil {
+		log.Fatal("ListenUnix failed:", err)
+	}
+
+}
---
+++ ssh-agent_windows.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Microsoft/go-winio"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+func ServeSSHAgentOnPipe(path string) error {
+	// Start a ssh agent on windows named pipe
+	var pipeCfg = &winio.PipeConfig{}
+	pipe, err := winio.ListenPipe(`\\.\pipe\openssh-ssh-agent`, pipeCfg)
+	if err != nil {
+		return err
+	}
+	defer pipe.Close()
+	log.Println("SSH agent started at ", path)
+	for {
+		conn, err := pipe.Accept()
+		if err != nil {
+			return err
+		}
+		go func() {
+			// Handle the connection
+			err := agent.ServeAgent(myAgent, conn)
+			log.Println("Agent conn closed:", err)
+		}()
+	}
+}
+
+func RunSSHServer() {
+	// Start a ssh agent on windows named pipe
+	err := ServeSSHAgentOnPipe(`\\.\pipe\openssh-ssh-agent`)
+	if err != nil {
+		log.Fatal("ListenPipe failed:", err)
+	}
+}
